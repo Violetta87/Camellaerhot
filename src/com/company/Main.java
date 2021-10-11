a1a50c9bd19496766d591368abbedaaf5fc4fb91
Camilla frandsen <camillafrandsen@Camillas-MacBook-Pro-3.local>
2021-10-11 11:43:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arrayone={1,2,3,4};
+        for (int x = 0; x < 4; x++) {
+            System.out.println("hello world"+ arrayone[x]);
+        }
+    }
+
+}
